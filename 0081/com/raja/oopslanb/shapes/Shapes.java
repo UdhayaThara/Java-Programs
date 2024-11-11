61e330c078c672e251905ba02d98ac3aab976654
UdhayaThara <udhayathara996@gmail.com>
2024-11-11 16:20:20 +0530

    Add files via upload

---
+++ 0081/com/raja/oopslanb/shapes/Shapes.java
@@ -0,0 +1,11 @@
+package com.raja.oopslanb.shapes;
+interface package com.raja.oopslanb.shapes;
+interface Shape {
+double length = 0.0;
+double hight = 0.0;
+public void printArea();
+} {
+double length = 0.0;
+double hight = 0.0;
+public void printArea();
+}
